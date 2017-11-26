79b0c2e180093b92167a35edf8d5cb5eff985453
ltthao <ltthao8996@gmail.com>
2017-11-26 08:41:13 +0700

    pull

--- YourTime/app/src/main/java/com/hcmute/trietthao/yourtime/CalendarFragment.java
+++ YourTime/app/src/main/java/com/hcmute/trietthao/yourtime/CalendarFragment.java
@@ -1,14 +1,44 @@
 package com.hcmute.trietthao.yourtime;
 
+import android.database.Cursor;
+import android.net.Uri;
+
 import com.hcmute.trietthao.yourtime.base.BaseFragment;
 import com.hcmute.trietthao.yourtime.calendar.WeekViewEvent;
+import com.hcmute.trietthao.yourtime.database.DBNguoiDungServer;
+import com.hcmute.trietthao.yourtime.database.DBWorkServer;
+import com.hcmute.trietthao.yourtime.database.GetWorkListener;
+import com.hcmute.trietthao.yourtime.model.CongViecModel;
+import com.hcmute.trietthao.yourtime.model.NguoiDungModel;
+import com.hcmute.trietthao.yourtime.prefer.PreferManager;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 
-public class CalendarFragment extends BaseFragment {
+public class CalendarFragment extends BaseFragment implements GetWorkListener, DBNguoiDungServer.userListener {
+
+    DBWorkServer dbWorkServer;
+    DBNguoiDungServer dbNguoiDungServer;
+    NguoiDungModel currentUser;
+    PreferManager preferManager;
+    ArrayList<CongViecModel> listWork;
+    CongViecModel congViecModel;
+    public static ArrayList<String> nameOfWork = new ArrayList<String>();
+    public static ArrayList<String> startDates = new ArrayList<String>();
+    public static ArrayList<String> endDates = new ArrayList<String>();
+    public static ArrayList<String> descriptions = new ArrayList<String>();
+    final int[] mColor = {
+            R.color.event_color_01,
+            R.color.event_color_02,
+            R.color.event_color_03,
+            R.color.event_color_04
+
+    };
+
 
     public static CalendarFragment newInstance() {
         CalendarFragment fragment = new CalendarFragment();
@@ -17,8 +47,43 @@ public class CalendarFragment extends BaseFragment {
 
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-        // Populate the week view with some events.
+
+        preferManager = new PreferManager(getActivity().getApplicationContext());
+        dbNguoiDungServer= new DBNguoiDungServer(this);
+        currentUser=new NguoiDungModel();
+        congViecModel= new CongViecModel();
+        HashMap<String, String> user = preferManager.getUserDetails();
+        dbNguoiDungServer.getUser(user.get(PreferManager.KEY_EMAIL));
+//        dbWorkServer.getListAllWork(currentUser.getIdNguoiDung());
+
+//        Cursor cursor = getContext().getContentResolver()
+//                .query(
+//                        Uri.parse("https://tlcn-yourtime.herokuapp.com/getlistallwork"),
+//                        new String[] { "idCongViec", "tenCongViec","thoiGianBatDau", "thoiGianKetThuc", "ghiChu",
+//                                 "fileDinhKem" }, null,
+//                        null, null);
+//        cursor.moveToFirst();
+//        String CNames[] = new String[cursor.getCount()];
+//
+//        // Populate the week view with some events.
+//        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+//
+//        nameOfWork.clear();
+//        startDates.clear();
+//        endDates.clear();
+//        descriptions.clear();
+//        for (int i = 0; i < CNames.length; i++) {
+//
+//            nameOfWork.add(cursor.getString(1));
+//            startDates.add(getDate(Long.parseLong(cursor.getString(2))));
+//            endDates.add(getDate(Long.parseLong(cursor.getString(3))));
+//            descriptions.add(cursor.getString(4));
+//            CNames[i] = cursor.getString(1);
+//            cursor.moveToNext();
+//
+//        }
         List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
 
         Calendar startTime = Calendar.getInstance();
         startTime.set(Calendar.HOUR_OF_DAY, 3);
@@ -31,6 +96,7 @@ public class CalendarFragment extends BaseFragment {
         WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
         event.setColor(getResources().getColor(R.color.event_color_01));
         events.add(event);
+
 
         startTime = Calendar.getInstance();
         startTime.set(Calendar.HOUR_OF_DAY, 3);
@@ -68,6 +134,7 @@ public class CalendarFragment extends BaseFragment {
         event = new WeekViewEvent(2, getEventTitle(startTime), startTime, endTime);
         event.setColor(getResources().getColor(R.color.event_color_02));
         events.add(event);
+        events.remove(event);
 
         startTime = Calendar.getInstance();
         startTime.set(Calendar.HOUR_OF_DAY, 5);
@@ -81,6 +148,7 @@ public class CalendarFragment extends BaseFragment {
         event = new WeekViewEvent(3, getEventTitle(startTime), startTime, endTime);
         event.setColor(getResources().getColor(R.color.event_color_03));
         events.add(event);
+        events.remove(event);
 
         startTime = Calendar.getInstance();
         startTime.set(Calendar.DAY_OF_MONTH, 15);
@@ -105,6 +173,7 @@ public class CalendarFragment extends BaseFragment {
         event = new WeekViewEvent(5, getEventTitle(startTime), startTime, endTime);
         event.setColor(getResources().getColor(R.color.event_color_01));
         events.add(event);
+        events.remove(event);
 
         startTime = Calendar.getInstance();
         startTime.set(Calendar.DAY_OF_MONTH, startTime.getActualMaximum(Calendar.DAY_OF_MONTH));
@@ -143,6 +212,7 @@ public class CalendarFragment extends BaseFragment {
         event = new WeekViewEvent(8, getEventTitle(startTime),null, startTime, endTime, true);
         event.setColor(getResources().getColor(R.color.event_color_03));
         events.add(event);
+        events.remove(event);
 
         // All day event until 00:00 next day
         startTime = Calendar.getInstance();
@@ -159,7 +229,37 @@ public class CalendarFragment extends BaseFragment {
         event.setColor(getResources().getColor(R.color.event_color_01));
         events.add(event);
 
+        events.remove(event);
         return events;
     }
 
+    @Override
+    public void getListAllWork(ArrayList<CongViecModel> congViecModelArrayList) {
+        listWork=congViecModelArrayList;
+
+    }
+
+    @Override
+    public void getListUser(ArrayList<NguoiDungModel> listUser) {
+
+    }
+
+    @Override
+    public void getResultInsert(Boolean isSuccess) {
+
+    }
+
+    @Override
+    public void getUser(NguoiDungModel user) {
+        currentUser=user;
+
+    }
+
+    public static String getDate(long milliSeconds) {
+        SimpleDateFormat formatter = new SimpleDateFormat(
+                "dd/MM/yyyy hh:mm:ss a");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
+    }
 }
